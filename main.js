cc0b2ed98ef07bbf624408842cb220d794b8663e
Simon <Simon_Freelance@hotmail.com>
2017-12-18 12:00:59 -0500

    character selection for human and computer completed

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
+const characters = document.getElementsByClassName("character-selection");
+const playerSpot = document.getElementById("player-spot");
+const computerSpot = document.getElementById("computer-spot");
+const startMatchBtn = document.getElementById("start-match-btn")
 
-function computerPlay() {
-  const choice = Math.floor(Math.random() * (3-0)+0);
-  const characters = ["rock","paper","scissors"];
-  return characters[choice];
+
+
+function computerChoice() {
+  const choice = Math.floor(Math.random() * characters.length);
+  computerSpot.src = characters[choice].src;
 }
+
+for (let i = 0; i < characters.length; i++) {
+
+  characters[i].addEventListener("click", function(event) {
+    playerSpot.src = characters[i].src;
+  });
+}
+startMatchBtn.addEventListener("click", function(event) {
+  computerChoice();
+});
